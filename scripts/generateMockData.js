9ecdbe3e5a9ae59616bc1fe9794866f0abd5fe63
Vincenzo <ilcontevince@gmail.com>
2022-04-24 22:20:47 +0100

    Change db.json location and add server.js

--- scripts/generateMockData.js
+++ scripts/generateMockData.js
@@ -1,12 +1,12 @@
 import * as fs from 'fs';
 import { mockData } from './schema.js';
 
-const dbFile = './db.json';
-console.log(!fs.existsSync(dbFile));
+const dbFile = './src/api/db.json';
+
 if (!fs.existsSync(dbFile)) {
   const json = JSON.stringify(mockData);
 
-  fs.writeFile(dbFile, json, function (err) {
+  fs.writeFile('./src/api/db.json', json, function (err) {
     if (err) {
       throw err;
     } else {
